bd8cb8d05305213bf78992c753f5c6af2072774c
celebrater <tkgm45nbchk@gmail.com>
2022-07-05 17:32:10 +0900

    created enemies to random position from the factory successfully

--- Assets/Scripts/Main/EnemiesFactoryController.cs
+++ Assets/Scripts/Main/EnemiesFactoryController.cs
@@ -8,12 +8,26 @@ public class EnemiesFactoryController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemiesShipPrefab);
+        InvokeRepeating("Spawn", 2f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(
+            Random.Range(-2.5f, 2.5f),
+            transform.position.y,
+            transform.position.z
+            );
+        Instantiate(
+            enemiesShipPrefab, 
+            spawnPosition, 
+            transform.rotation
+        );
     }
 }
